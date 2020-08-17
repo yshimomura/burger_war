a6b4d4a4f3a6a9ed406c4ca3124ca77770989457
shimo <shimomura.yusuke001@jp.panasonic.com>
2020-08-18 07:47:26 +0900

    add avoid obstacle & modify strategy

--- burger_war/scripts/randomRun.py
+++ burger_war/scripts/randomRun.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
-This is rumdom run node.
-subscribe 'scan' topcs.
-Publish 'cmd_vel' topic. 
-mainly use for simple sample program
-
-by Takuya Yamaguhi.
-'''
 
 import rospy
 import random
 import numpy as np
 import tf
+import cv2
+import sys
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan, Image
 from aruco_msgs.msg import MarkerArray
+from cv_bridge import CvBridge, CvBridgeError
 from nav_msgs.msg import Odometry
 import actionlib #topic通信においてクライアントからの返信を要求するライブラリ
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
@@ -25,11 +20,8 @@ class RandomBot():
     def __init__(self, bot_name="NoName"):
         # bot name 
         self.name = bot_name
-
         #wall distance
-        self.near_wall_range = 0.2  # [m]
-        # speed [m/s]
-        self.speed = 0.1
+        self.near_wall_range = 0.15  # [m]
         # lidar scan
         self.scan = []
         # target_id
@@ -38,39 +30,53 @@ class RandomBot():
         self.pose_x = 0
         self.pose_y = 0
         self.th = 0
+        # speed [m/s]
+        self.speed = 0.12
+
+        self.bridge = CvBridge()
+        rospy.on_shutdown(self.cleanup)
 
         # publisher
         self.vel_pub = rospy.Publisher('cmd_vel', Twist,queue_size=1)
         # subscriber
-        """self.lidar_sub = rospy.Subscriber('scan',LaserScan,self.LidarCallback)"""
+        self.lidar_sub = rospy.Subscriber('scan',LaserScan,self.LidarCallback)
         self.id_sub = rospy.Subscriber('target_id',MarkerArray,self.IdCallback)
-        self.pose_sub = rospy.Subscriber('odom', Odometry, self.poseCallback)
+        self.pose_sub = rospy.Subscriber('odom', Odometry, self.PoseCallback)
+        self.image_sub = rospy.Subscriber('image_raw', Image, self.ImageCallback)
         #actionlib
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
 
         self.twist = Twist()
-        self.twist.linear.x = self.speed; self.twist.linear.y = 0.; self.twist.linear.z = 0.
+        self.twist.linear.x = 0; self.twist.linear.y = 0.; self.twist.linear.z = 0.
         self.twist.angular.x = 0.; self.twist.angular.y = 0.; self.twist.angular.z = 0.
-    """
+
     def LidarCallback(self,data):
         scan = data.ranges
         self.scan = scan
         near_wall = self.NearWall(scan)
-        if near_wall:
+        if near_wall==1:
             self.twist.linear.x = -self.speed / 2
+            self.twist.angular.z = 0
+        elif near_wall==2:
+            self.twist.linear.x = self.speed / 2
+            self.twist.angular.z = 0
         else:
             self.twist.linear.x = self.speed
 
     def NearWall(self, scan):
         if not len(scan) == 360:
             return False
-        forword_scan = scan[:15] + scan[-15:]
+        forword_scan = scan[:20] + scan[-20:]
+        back_scan = scan[160:200]
         # drop too small value ex) 0.0
         forword_scan = [x for x in forword_scan if x > 0.1]
-        if min(forword_scan) < 0.2:
-            return True
+        back_scan = [x for x in back_scan if x > 0.1]
+        if min(forword_scan) < self.near_wall_range:
+            return 1
+        elif min(back_scan) < self.near_wall_range:
+            return 2
         return False
-        """
+
     def IdCallback(self, data):
         id = data.markers[0].id
         if self.id_list[-1]!=id:
@@ -78,7 +84,7 @@ class RandomBot():
         else:
             pass
 
-    def poseCallback(self, data):
+    def PoseCallback(self, data):
         '''
         pose topic from amcl localizer
         update robot twist
@@ -108,46 +114,52 @@ class RandomBot():
         """
         print(self.pose_x,self.pose_y,self.th)
 
-    def calcTwist(self):
-        value = random.randint(1,1000)
-        if value < 300:
-            x = 0.3
-            th = 0
-        elif value < 500:
-            x = -0.2
-            th = 0
-        elif value < 750:
-            x = 0
-            th = 0.3
-        elif value < 1000:
-            x = 0
-            th = -0.3
+    def ImageCallback(self, image):
+        try:
+            frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
+        except CvBridgeError, e:
+            print e
+        input_image = np.array(frame, dtype=np.uint8)
+
+        rects = self.process_image(input_image)
+        print(rects)
+        if rects!=[]:
+            if rects[0][0] + rects[0][2]/2 < 220:
+                self.twist.linear.y = 0; self.twist.linear.z = 0
+                self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = 0.3
+            elif rects[0][0] + rects[0][2]/2 > 260:
+                self.twist.linear.y = 0; self.twist.linear.z = 0
+                self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = -0.3
+            else:
+                self.twist.linear.y = 0; self.twist.linear.z = 0
+                self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = 0
         else:
-            x = 0
-            th = 0
-        self.twist = Twist()
-        self.twist.linear.x = x; self.twist.linear.y = 0; self.twist.linear.z = 0
-        self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = th
-        return self.twist
+            self.twist.linear.x = -0.1; self.twist.linear.y = 0; self.twist.linear.z = 0
+            self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = 0
+        cv2.waitKey(1)
+
+    def process_image(self, image):
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
+        h = hsv[:, :, 0]
+        s = hsv[:, :, 1]
+        mask = np.zeros(h.shape, dtype=np.uint8)
+        mask[((h < 20) | (h > 200)) & (s > 128)] = 255
+        _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        rects = []
+        for contour in contours:
+            approx = cv2.convexHull(contour)
+            rect = cv2.boundingRect(approx)
+            rects.append(np.array(rect))
+        return rects
+    
+    def cleanup(self):
+        cv2.destroyAllWindows() 
 
     def FirstPoint(self):
-        self.twist = Twist()
-        self.twist.linear.x = 0.3; self.twist.linear.y = 0; self.twist.linear.z = 0
+        self.twist.linear.x = 0.2; self.twist.linear.y = 0; self.twist.linear.z = 0
         self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = 0
         return self.twist
     
-    def SecondPoint(self):
-        self.twist = Twist()
-        if self.pose_y>-0.7:
-            self.twist.linear.x = -0.3; self.twist.linear.y = 0; self.twist.linear.z = 0
-            self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = 0
-        elif self.th>1:
-            self.twist.linear.x = 0; self.twist.linear.y = 0; self.twist.linear.z = 0
-            self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = 0.5
-        else:
-            self.calcTwist()
-        return self.twist
-
     def setGoal(self,x,y,yaw):
         self.client.wait_for_server()
 
@@ -173,37 +185,20 @@ class RandomBot():
             return self.client.get_result()
 
     def strategy(self):
-        r = rospy.Rate(3) # change speed 1fps
-
-        self.setGoal(-0.5,0,0)
-        self.setGoal(-0.5,0,3.1415/2)
-        self.setGoal(0,0.5,0)
-        self.setGoal(0,0.5,3.1415)
-        self.setGoal(-0.5,0,-3.1415/2)
-        self.setGoal(0,-0.5,0)
-        self.setGoal(0,-0.5,3.1415)
-        """
-        target_speed = 0
-        target_turn = 0
-        control_speed = 0
-        control_turn = 0
-
+        r = rospy.Rate(5) # change speed 1fps
+        flag = 0
         while not rospy.is_shutdown():
             if self.id_list[-1]==0:
                 twist_first = self.FirstPoint()
                 print(twist_first)
                 self.vel_pub.publish(twist_first)
-            elif self.id_list[-1]==2:
-                twist_second = self.SecondPoint()
-                print(twist_second)
-                self.vel_pub.publish(twist_second)
+            elif flag==0:
+                self.setGoal(0.2,0.5,-np.pi/4)
+                flag = 1
             else:
-                twist = self.calcTwist()
-                print(twist)
-                print(self.id_list)
-                self.vel_pub.publish(twist)
+                self.vel_pub.publish(self.twist)
+            print(self.twist)
             r.sleep()
-            """
 
 if __name__ == '__main__':
     rospy.init_node('random_run')
